ea99e96d1388399fd08c0b075d8eca8671d90750
Techno-Miz <muzzu2488@gmail.com>
2023-10-06 17:22:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <h1>School Days</h1>
+    <div className="main">
+      <h5>School Friends</h5>
+      <ol>
+        <li>Tonny</li>
+        <li>Martin</li>
+        <li>Robert</li>
+      </ol>
+    </div>
+    <div className="sub">
+      <h5>School Routine</h5>
+      <ol type="a">
+        <li>I used to woke up early in the morning</li>
+        <li>I use to take bath</li>
+        <li>I use to take Breakfast</li>
+        <li>I use to catchs my school bus</li>
+        <li>I use to enjoy my school</li>
+        <li>I use to catch my bus to come to home</li>
+      </ol>
+    </div>
+   </div>
+  );
+}
+
+export default App;
